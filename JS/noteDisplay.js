ff9623391c8bd6ff03343a85e7183d6e0db993f3
polon59 <przemyslaw.polczak@gmail.com>
2018-10-10 13:16:50 +0200

    Implement function 'displayNote' in noteDisplay.js

--- JS/noteDisplay.js
+++ JS/noteDisplay.js
@@ -1,12 +1,22 @@
 function displayNote(note) {
-    var title = "new Title"
+    var title = "new Title";
+    var content = "new Content";
+    var htmlString = render(title,content);
 
+
+
+    var newNote = document.createElement('div');
+    newNote.innerHTML = htmlString.trim();
+
+
+    var element = document.getElementById("noteContainer");
+    element.appendChild(newNote);
 }
 
 
 
 function render(title,content) {
-    `<div class="note">
+    return `<div class="note">
         <div class="panel-heading" draggable="true">
             <h3 class="noteTitle">${title}</h3>
             <div class="deleteButton">
